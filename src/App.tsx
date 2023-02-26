d52e736fa2265f37962558ad3de0cce293e86c9b
hansfigo <hambasahaya0303@gmail.com>
2023-02-26 18:16:02 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,6 @@ import MyHeader from './components/Header';
 
 function App() {
 
-  console.log(import.meta.env.VITE_API_KEY)
-
-
   return (
     <Container pos={'relative'} maxW={['', '', '1740px']}>
       <Router>
