f3172947842f9cdfeb9a0ee4beefa8baae13a8d9
hansfigo <hambasahaya0303@gmail.com>
2023-03-10 23:49:44 +0700

    add_refresh_btn

--- src/components/AnimePlayerPage/VideoPlayer.tsx
+++ src/components/AnimePlayerPage/VideoPlayer.tsx
@@ -2,54 +2,56 @@ import React, { useState, useEffect } from 'react';
 import { Box, Button, Center, Stack, Text, VStack } from '@chakra-ui/react';
 import ReactPlayer from 'react-player';
 import { useParams } from 'react-router-dom';
-import { log } from 'console';
 
 interface videoPlayerProps {
-    episodeId: string
+  refresh: boolean;
 }
 
-const VideoPlayer = () => {
-    const [videoUrl, setVideoUrl] = useState('');
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+const VideoPlayer = ({ refresh }: videoPlayerProps) => {
+  const [videoUrl, setVideoUrl] = useState('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isReady, setIsReady] = useState<boolean>(false);
+  const { episodeId } = useParams<{ episodeId: string }>();
 
-    const { episodeId } = useParams<{ episodeId: string }>();
+  useEffect(() => {
+    async function fetchVideoData() {
+      try {
+        setIsLoading(true);
+        const url = `https://api.consumet.org/anime/gogoanime/watch/${episodeId}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setVideoUrl(data.sources[4].url);
+        setIsLoading(false);
+        setIsReady(true);
+      } catch (error) {
+        console.error('error');
+      }
+    }
 
+    fetchVideoData();
+  }, [episodeId, refresh]);
 
+  if (isLoading) {
+    return <Text>Loading</Text>;
+  }
 
+  if (!isReady) {
+    return null;
+  }
 
-    useEffect(() => {
-        async function fetchVideoData() {
-            try {
-                setIsLoading(true);
-                const url = `https://api.consumet.org/anime/gogoanime/watch/${episodeId}`
-                const response = await fetch(url);
-                const data = await response.json();
-                setVideoUrl(data.sources[4].url);
-                console.log(videoUrl)
-                setIsLoading(false)
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
-        fetchVideoData();
-    }, []);
+  try {
     return (
-        <Box>
-            {
-                !isLoading ? <ReactPlayer
-                    url={`https://cors.haikei.xyz/${videoUrl}`}
-                    controls
-                    width="100%"
-                    height="auto"
-                    onError={(e) => console.log(e)}
-                /> :
-                    <Text>Loading</Text>
-
-            }
-        </Box>
-    )
-
-}
+      <ReactPlayer
+        url={`https://cors.haikei.xyz/${videoUrl}`}
+        controls
+        width="100%"
+        height="auto"
+        onError={(e) => console.log(e)}
+      />
+    );
+  } catch (error) {
+    return <Text>Error: {'err'}</Text>;
+  }
+};
 
 export default VideoPlayer;
--- src/pages/AnimePlayerPage.tsx
+++ src/pages/AnimePlayerPage.tsx
@@ -1,10 +1,12 @@
 import { Box, Text, Heading, Flex, Button, Image } from "@chakra-ui/react";
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import VideoPlayer from "../components/AnimePlayerPage/VideoPlayer"
 
 const AnimePlayer = () => {
 
     const { animeId, episodeId } = useParams();
+    const [refresh, setRefresh] = useState(false);
 
     const episodeName = episodeId?.replace(/-/g, ' ').toLowerCase().replace(/\b\w/g, c => c.toUpperCase())
     const animeTitle = animeId?.replace(/-/g, ' ').toLowerCase().replace(/\b\w/g, c => c.toUpperCase())
@@ -12,8 +14,9 @@ const AnimePlayer = () => {
         <Flex flexDir={{ base: 'column' }} gap={4}>
             <Box>
                 <Box maxW={'4xl'}>
-                    <VideoPlayer />
+                    <VideoPlayer refresh =  {refresh} />
                 </Box>
+                <Button pt={2} onClick={()=>setRefresh(!refresh)}>Refresh ?</Button>
                 <Heading maxW={'4xl'} fontSize={{ base: '2xl' }} noOfLines={2} pt='8'>{episodeName}</Heading>
             </Box>
             <Link to={`/anime/${animeId}`}>
